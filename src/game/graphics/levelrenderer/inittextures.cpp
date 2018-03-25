a69de5de9fe194af0ab078a18047e3ac80ab15c9
lahvdbrand <l.a.h.van.den.brand@student.rug.nl>
2018-03-25 12:35:29 +0200

    Added 5 new newspapers, now all 7 levels are represented.

--- src/game/graphics/levelrenderer/inittextures.cpp
+++ src/game/graphics/levelrenderer/inittextures.cpp
@@ -5,9 +5,14 @@ bool LevelRenderer::initTextures(TextureFactory &textureFactory)
   // Try to load the newspaper textures.
   try
   {
-    d_newspapers.reserve(2);
+    d_newspapers.reserve(7);
     d_newspapers.push_back(textureFactory.texture("data/level0.png"));
     d_newspapers.push_back(textureFactory.texture("data/level1.png"));
+    d_newspapers.push_back(textureFactory.texture("data/level2.png"));
+    d_newspapers.push_back(textureFactory.texture("data/level3.png"));
+    d_newspapers.push_back(textureFactory.texture("data/level4.png"));
+    d_newspapers.push_back(textureFactory.texture("data/level5.png"));
+    d_newspapers.push_back(textureFactory.texture("data/level6.png"));
   }
   catch (invalid_argument &invalidArgument)
   {
